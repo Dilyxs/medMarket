63af09b5d891b3a4f5a25606ed106e5465ebf3d6
Adsayan Selvankumaran <selvan@adsayan.com>
2026-01-17 16:09:02 -0500

    added core vidoe streaming logic, yet to implement handle

---
+++ server/pkg/FeedForwarder.go
@@ -0,0 +1,197 @@
+package pkg
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+type UserViewer struct {
+	ID                        int
+	Conn                      *websocket.Conn
+	UserReceivingVideoDetails chan VideoFrameWithAnnotations
+}
+type Broadcaster struct {
+	ID                      int
+	Conn                    *websocket.Conn
+	UserReadingVideoDetails chan VideoFrameWithAnnotations
+}
+
+type Rectange struct {
+	X      int
+	Y      int
+	Width  int
+	Height int
+}
+
+type Dot struct {
+	X      int
+	Y      int
+	Radius int
+}
+
+type VideoFrameWithAnnotations struct {
+	Frame       []byte       `json:"frame"`
+	Annotations []Annotation `json:"annotations"`
+}
+
+type Annotation struct {
+	Color string
+	Rect  Rectange
+	Dot   Dot
+}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+type BroadcastServerHub struct {
+	Viewers                               map[int]*UserViewer
+	VideoDetailsChan                      chan VideoFrameWithAnnotations
+	EndOFStream                           chan bool
+	ListenForIncomingUserOrDisconnections chan *UserViewerAddition
+	Mu                                    sync.RWMutex
+}
+type UserViewerAddition struct {
+	User       *UserViewer
+	WantsToAdd bool
+}
+
+func AddNewUserViewerToHub(hub *BroadcastServerHub, w http.ResponseWriter, r *http.Request, viewerID int) {
+	w.Header().Set("Content-Type", "application/json")
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		w.WriteHeader(http.StatusUpgradeRequired)
+		w.Write([]byte(`{"error": "WebSocket upgrade failed"}`))
+	}
+	VideoUser := &UserViewer{
+		ID:                        viewerID,
+		Conn:                      conn,
+		UserReceivingVideoDetails: make(chan VideoFrameWithAnnotations, 1000),
+	}
+	hub.ListenForIncomingUserOrDisconnections <- &UserViewerAddition{
+		User:       VideoUser,
+		WantsToAdd: true,
+	}
+	go VideoUser.ListenForVideoDetails()
+	go VideoUser.ReadPump(hub)
+}
+
+func (v *UserViewer) ReadPump(hub *BroadcastServerHub) {
+	defer func() {
+		v.Conn.Close()
+		hub.ListenForIncomingUserOrDisconnections <- &UserViewerAddition{
+			User:       v,
+			WantsToAdd: false,
+		}
+	}()
+	for {
+		if _, _, err := v.Conn.ReadMessage(); err != nil { // ok so frontend will always send in some pings and other stuff, just ignore that stuff
+			break
+		}
+	}
+}
+
+func (b *BroadcastServerHub) AddOrRemoveUser() {
+	for incomingUser := range b.ListenForIncomingUserOrDisconnections {
+		b.Mu.Lock()
+		if incomingUser.WantsToAdd {
+			b.Viewers[incomingUser.User.ID] = incomingUser.User
+		} else {
+			delete(b.Viewers, incomingUser.User.ID)
+		}
+		b.Mu.Unlock()
+	}
+}
+
+func (v *UserViewer) ListenForVideoDetails() {
+	for message := range v.UserReceivingVideoDetails {
+		if err := v.Conn.WriteJSON(message); err != nil { // happens if user ends the session
+			return
+		}
+	}
+}
+
+func (b *BroadcastServerHub) EnndBroadcastingSession() {
+	for range b.EndOFStream {
+		b.Mu.RLock()
+		for _, viewer := range b.Viewers {
+			if err := viewer.Conn.WriteMessage(websocket.TextMessage, []byte("stream has ended")); err != nil { // happens if user
+				continue
+			}
+		}
+		b.Mu.RUnlock()
+	}
+}
+
+func (b *Broadcaster) ConnectBroadCaster(hub *BroadcastServerHub, w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		w.WriteHeader(http.StatusUpgradeRequired)
+		if _, ConnErr := w.Write([]byte(`{"error": "WebSocket upgrade failed"}`)); ConnErr != nil { // this happens cause user left the page or something
+			hub.EndOFStream <- true
+		}
+	}
+	defer func() {
+		err := conn.Close()
+		if err != nil { // this happens if the user just leaves the page or something and conn is lost!
+			select {
+			case hub.EndOFStream <- true:
+			default:
+			}
+		}
+	}()
+	Broadcaster := &Broadcaster{
+		Conn:                    conn,
+		UserReadingVideoDetails: make(chan VideoFrameWithAnnotations, 1000),
+	}
+	Broadcaster.ListenForVideoInput(hub)
+}
+
+func (b *Broadcaster) ListenForVideoInput(hub *BroadcastServerHub) {
+	var newMessage VideoFrameWithAnnotations
+	for {
+		err := b.Conn.ReadJSON(&newMessage)
+		if err != nil {
+			fmt.Println("cannot decode video")
+			hub.EndOFStream <- true
+			return
+		}
+		hub.VideoDetailsChan <- newMessage
+	}
+}
+
+func (b *BroadcastServerHub) ShareBroadscastingDetails() {
+	for message := range b.VideoDetailsChan {
+		b.Mu.RLock()
+		for _, viewer := range b.Viewers {
+			select {
+			case viewer.UserReceivingVideoDetails <- message:
+			default:
+			}
+		}
+		b.Mu.RUnlock()
+	}
+}
+
+func NewBroadcastServerHub() *BroadcastServerHub {
+	return &BroadcastServerHub{
+		Viewers:                               make(map[int]*UserViewer),
+		VideoDetailsChan:                      make(chan VideoFrameWithAnnotations, 1000),
+		EndOFStream:                           make(chan bool),
+		ListenForIncomingUserOrDisconnections: make(chan *UserViewerAddition, 1000),
+	}
+}
+
+func (b *BroadcastServerHub) StartHubWork() {
+	go b.AddOrRemoveUser()
+	go b.ShareBroadscastingDetails()
+	go b.EnndBroadcastingSession()
+}
